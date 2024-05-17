f770d10c3d0d5da6c59ca95a0d7d4815d2006a3b
j9972 <j9972@naver.com>
2024-05-17 22:08:24 +0900

    feat : init setting

---
+++ src/main/java/com/example/policyspoon/base/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.example.policyspoon.base.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode, String msg) {
+        super(errorCode.getMessage() + " : " + msg);
+        this.errorCode = errorCode;
+    }
+}
---
+++ src/main/java/com/example/policyspoon/base/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.policyspoon.base.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception exception) {
+        log.warn("[Exception] occurs : ", exception);
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse("UNEXPECTED_ERROR", "some errors occurred : " + exception.getMessage()));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        StackTraceElement element = exception.getStackTrace()[0];
+        log.warn("[{}] occurs caused by {}.{}() {} line : {}", errorCode.name(), element.getClassName(), element.getMethodName(), element.getLineNumber(), exception.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.name(), exception.getMessage()));
+    }
+
+    public record ErrorResponse(String code, String message) {
+    }
+}
