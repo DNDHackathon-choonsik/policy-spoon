cfbfb62d497205a8905b89facfeed707811c437f
j9972 <j9972@naver.com>
2024-05-17 21:43:21 +0900

    init

---
+++ src/main/java/com/example/policyspoon/PolicySpoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.policyspoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicySpoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicySpoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/policyspoon/PolicySpoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.policyspoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicySpoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
